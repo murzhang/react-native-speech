777abae9c06c5414aa2777e51ea98a51de35cd4a
murzhang <121521977@qq.com>
2019-02-21 12:01:31 +0800

    根据各平台修改语速标准
    ios 标准值0.5，languange:zh-CN 男声 zh-TW 女声
    android 标准中1.0

--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
-import { NativeModules } from 'react-native';
-const { SpeechModule } = NativeModules;
+import {NativeModules, Platform} from 'react-native';
+
+const {SpeechModule} = NativeModules;
 
 const Speech = {
-  speak(context, callback) {
-    let options = {};
-    if (context.constructor === String) {
-      options.text = context;
-      options.language = 'zh-CH';
-    } else {
-      options = context;
+    speak(context, callback) {
+        let options = {};
+        if (context.constructor === String) {
+            options.text = context;
+            options.language = 'zh-CH';
+        } else {
+            options = context;
+        }
+        if (!callback) {
+            callback = function callback(argument) {
+            }
+        }
+        let platformStandRate = 1.0;
+        if (Platform.OS === 'ios') {
+            platformStandRate = 0.5;
+        }
+        options.rate = options.rate || 1.0;
+        options.rate = options.rate * platformStandRate;//换算为平台标准语速
+        SpeechModule.speak(options, callback);
+    },
+    stop() {
+        SpeechModule.stop();
+    },
+    isSpeaking(callback) {
+        if (!callback) {
+            callback = function callback(argument) {
+            }
+        }
+        SpeechModule.isSpeaking(callback);
     }
-    if (!callback) {
-      callback = function callback(argument) { }
-    }
-    SpeechModule.speak(options, callback);
-  },
-  stop() {
-    SpeechModule.stop();
-  },
-  isSpeaking(callback) {
-    if (!callback) {
-      callback = function callback(argument) { }
-    }
-    SpeechModule.isSpeaking(callback);
-  }
 }
 
 export default Speech;
