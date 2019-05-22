d8c6acbb273870babe34d2c180f8bb94c8549080
murzhang <121521977@qq.com>
2019-05-22 17:47:49 +0800

    android还是通过独立底层来实现播报
    考虑到安卓系统差异性太大，很多没有TTS中文引擎，导致语音播报失败

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import {NativeModules, Platform} from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
-const {SpeechModule} = NativeModules;
+const { SpeechModule } = NativeModules;
 
 const Speech = {
     speak(context, callback) {
@@ -18,20 +18,83 @@ const Speech = {
         let platformStandRate = 1.0;
         if (Platform.OS === 'ios') {
             platformStandRate = 0.5;
+            options.rate = options.rate || 1.0;
+            options.rate = options.rate * platformStandRate;//换算为平台标准语速
         }
-        options.rate = options.rate || 1.0;
-        options.rate = options.rate * platformStandRate;//换算为平台标准语速
-        SpeechModule.speak(options, callback);
+        else {
+            options.rate = options.rate || 1.0;
+            /* SpeechModule 规则
+            if (options.rate == 0.25) {
+                options.rate = 0.25;
+            }
+            else if (options.rate == 0.5) {
+                options.rate = 0.5;
+            }
+            else if (options.rate == 0.75) {
+                options.rate = 0.75;
+            }
+            else if (options.rate == 1.0) {
+                options.rate = 1.0;
+            }
+            else if (options.rate == 1.25) {
+                options.rate = 3.5;
+            }
+            else if (options.rate == 1.5) {
+                options.rate = 7.5;
+            }
+            else if (options.rate == 1.75) {
+                options.rate = 30.0;
+            }
+            */
+           // YSModule 规则0~100
+            if (options.rate == 0.25) {
+                options.rate = 15.0;
+            }
+            else if (options.rate == 0.5) {
+                options.rate = 25.0;
+            }
+            else if (options.rate == 0.75) {
+                options.rate = 35.0;
+            }
+            else if (options.rate == 1.0) {
+                options.rate = 50.0;
+            }
+            else if (options.rate == 1.25) {
+                options.rate = 75.0;
+            }
+            else if (options.rate == 1.5) {
+                options.rate = 85.0;
+            }
+            else if (options.rate == 1.75) {
+                options.rate = 95.0;
+            }
+        }
+        if (Platform.OS === "ios") {
+            SpeechModule.speak(options, callback);
+        }
+        else {
+            NativeModules.YSTTS.speak(options, callback);
+        }
     },
     stop() {
-        SpeechModule.stop();
+        if (Platform.OS === "ios") {
+            SpeechModule.stop();
+        }
+        else {
+            NativeModules.YSTTS.stop();
+        }
     },
     isSpeaking(callback) {
         if (!callback) {
             callback = function callback(argument) {
             }
         }
-        SpeechModule.isSpeaking(callback);
+        if (Platform.OS === "ios") {
+            SpeechModule.isSpeaking(callback);
+        }
+        else {
+            NativeModules.YSTTS.isSpeaking(callback);
+        }
     }
 }
 
